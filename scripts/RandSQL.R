0cbe79d20af7896e76e86f05132017895307cf7f
orchid00 <orchid00@gmail.com>
2018-06-13 12:12:27 +0200

    add files

--- scripts/RandSQL.R
+++ scripts/RandSQL.R
@@ -42,28 +42,31 @@ download.file(url = "https://ndownloader.figshare.com/files/2292171",
               destfile = "data/portal_mammals.sqlite", mode = "wb")
 # Create connection to the sqlite database ----
 # The RSQLite package allows R to interface with SQLite databases. 
-mammals <- DBI::dbConnect(RSQLite::SQLite(), "data/portal_mammals.sqlite")
+DBconnection <- DBI::dbConnect(RSQLite::SQLite(), "data/portal_mammals.sqlite")
 # Important this SQLite function does not load the data into the R session
 # It is like an open window to your SQLite database
 # check 
-str(mammals) # is a class SQLiteConnection
+str(DBconnection) # is a class SQLiteConnection
 
 
 
-# Take a look at the database mammals ----
+# Take a look at the database DBconnection ----
 # ?src_dbi #  from dbplyr is an interface to use the connection and read
 # the information from the database 
-src_dbi(mammals)
+src_dbi(DBconnection)
 # let's work with SQL syntax using tbl to create a table ----
-src_dbi(mammals)
-tbl(mammals, sql("SELECT year, species_id, plot_id, weight 
+tbl(DBconnection, "surveys")
+tbl(DBconnection, "plots")
+
+tbl(DBconnection, sql("SELECT year, species_id, plot_id, weight 
                   FROM surveys
                  ORDER BY weight DESC"))
 # what have we done here?
 # Yes! we can use all of the queries that we wrote on the SQL lesson
 # dbplyr syntax ----
 # About sections https://support.rstudio.com/hc/en-us/articles/200484568-Code-Folding-and-Sections
-surveys <- tbl(mammals, "surveys")
+surveys <- tbl(DBconnection, "surveys")
+
 surveys %>% 
     select(year, species_id, plot_id, weight)
 
@@ -80,10 +83,18 @@ nrow(surveys)
 show_query(head(surveys))
 
 # does this work with other dplyr chunks?
-filter(surveys, year > 1995)
+surveys %>% 
+   filter(year > 1995)
+#filter(surveys, year > 1995)
 show_query(filter(surveys, year > 1995))
 # dplyr does the SQL query for you
-# 
+
+surveys %>% 
+    head(n = 12)
+
+show_query(surveys %>% 
+               head(n = 12))
+
 # Challenge using dplyr
 # 1. Select the species_id, sex and weight from surveys
 # where weight is lower than 5
@@ -94,12 +105,12 @@ surveys %>%
     arrange(weight)
 # wait ... with more rows
 
-plots <- tbl(mammals, "plots")
+plots <- tbl(DBconnection, "plots")
 plots %>%
     filter(plot_id == 1) %>%
     inner_join(surveys) %>%
   collect()
 
-format_csv(plots)
-write_csv(plots, file.path("data", "surveys_exported.csv"))
+myplots <- format_csv(plots)
+write_csv(plots, file.path("data", "plots_exported.csv"))
 
--- scripts/newDB.R
+++ scripts/newDB.R
@@ -1,14 +1,22 @@
-species <- read_csv("data/species.csv")
+library(dbplyr)
+library(DBI)
+library(RSQLite)
 
-surveys <- read_csv("data/surveys.csv")
+species_o <- read_csv("data/species.csv")
 
-plots <- read_csv("data/plots.csv")
+surveys_o <- read_csv("data/surveys.csv")
+
+plots_o <- read_csv("data/plots.csv")
 
 
 my_db_file <- "data/portal-database_DC.sqlite"
 my_db <- src_sqlite(my_db_file, create = TRUE)
 
 # add tables
-copy_to(my_db, surveys)
-copy_to(my_db, plots)
+copy_to(my_db, surveys_o, "surveys")
+copy_to(my_db, plots_o, "plots")
+copy_to(my_db, species_o, "species")
 my_db
+
+
+# now we finish this part but you can follow the steps on the previous 
