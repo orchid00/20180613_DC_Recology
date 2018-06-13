d9abcbe0bdf4187ba5c51a817a1727004e19f04f
orchid00 <orchid00@gmail.com>
2018-06-13 12:08:04 +0200

    add script

--- scripts/startingWithData.R
+++ scripts/startingWithData.R
@@ -1,2 +1,10 @@
 # load package tidiverse
 library(tidyverse)
+
+download.file("https://ndownloader.figshare.com/files/2292169",
+              "data/portal_data_joined.csv")
+
+surveysdb <- read_csv("data/portal_data_joined.csv")
+
+glimpse(surveysdb)
+str(surveysdb)
